c0ec5662b24ee94f1f69913726f3a471e2c8555a
XiaoFeng Gong <20091570@wit.ie>
2020-12-16 16:23:29 +0000

    Topic 10 - Passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -7,7 +7,8 @@ import './db';
 import {loadUsers} from './seedData';
 import usersRouter from './api/users';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
+
 dotenv.config();
 
 const errHandler = (err, req, res, next) => {
@@ -22,6 +23,8 @@ if (process.env.SEED_DB) {
   loadUsers();
 }
 const app = express();
+// initialise passport​
+app.use(passport.initialize());;
 
 const port = process.env.PORT;
 //configure body-parser
@@ -47,5 +50,5 @@ app.listen(port, () => {
 app.use('/api/users', usersRouter);
 app.use(errHandler);
 
-//update /api/Movie route
-app.use('/api/movies', authenticate, moviesRouter);
+// Add passport.authenticate(..)  to middleware stack for protected routes​
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
